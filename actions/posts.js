61d3b9767c7ea6ebdc31ff3faf83773c6dfeb1da
Rafał Curzydło <rafall.curzydlo@gmail.com>
2025-11-04 19:56:11 +0100

    Add optimistic UI updates for post likes and revalidation on like status toggle

--- actions/posts.js
+++ actions/posts.js
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 
 import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(prevState, formData) {
   const title = formData.get("title");
@@ -43,9 +44,11 @@ export async function createPost(prevState, formData) {
     userId: 1,
   });
 
+  revalidatePath("/", "layout");
   redirect("/feed");
 }
 
 export async function tooglePostLikeStatus(postId, formData) {
-  updatePostLikeStatus(postId, 2);
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
 }
